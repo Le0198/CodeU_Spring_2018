896e4526fabe0f75cae631da41301e788b6a0d0b
Jessica <zhux0551@umn.edu>
2018-06-09 21:13:33 -0500

    removing unnecessary things

--- src/main/java/codeu/controller/LogoutServlet.java
+++ src/main/java/codeu/controller/LogoutServlet.java
@@ -14,8 +14,6 @@
 
 package codeu.controller;
 
-import codeu.model.data.User;
-import codeu.model.store.basic.UserStore;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,9 +23,6 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet class responsible for the logout page. */
 public class LogoutServlet extends HttpServlet {
 
-  /** Store class that gives access to Users. */
-  private UserStore userStore;
-
   /**
    * Set up state for handling login-related requests. This method is only called when running in a
    * server, not when running in a test.
@@ -35,15 +30,6 @@ public class LogoutServlet extends HttpServlet {
   @Override
   public void init() throws ServletException {
     super.init();
-    setUserStore(UserStore.getInstance());
-  }
-
-  /**
-   * Sets the UserStore used by this servlet. This function provides a common setup method for use
-   * by the test framework or the servlet's init() function.
-   */
-  void setUserStore(UserStore userStore) {
-    this.userStore = userStore;
   }
 
   /**
@@ -60,7 +46,7 @@ public class LogoutServlet extends HttpServlet {
         request.getSession().setAttribute("user", null);
         response.sendRedirect("/index.jsp");
   }
-  
+
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
